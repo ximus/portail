22cedb81bc3aa87de3169488ca37716cebb36025
ximus <maximeliron@gmail.com>
2015-06-03 18:08:58 -0700

    Refactor header file

--- shared/portail.h
+++ shared/portail.h
@@ -1,2 +1,13 @@
+#ifndef PORTAIL_H__
+#define PORTAIL_H__
+
+
+#include "cc110x_legacy.h"
+
 #define GATE_PROXY_RADIO_ADDR   (1)
 #define GATEWAY_RADIO_ADDR      (2)
+
+#define PORTAIL_MAX_DATA_SIZE (CC1100_MAX_DATA_LENGTH)
+
+
+#endif /* end of include guard: PORTAIL_H__ */
