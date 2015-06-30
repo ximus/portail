26617abe06d96a55f8dcb4b00dac9060ba1b40ad
ximus <maximeliron@gmail.com>
2015-06-30 10:02:06 -0700

    Add project tools

---
+++ tools/stat-map-file.rb
@@ -0,0 +1,35 @@
+require 'csv'
+require 'pry'
+require 'terminal-table'
+
+path = ARGV[0]
+path && path.size > 0 or raise("usage: stat-map-file.rb [path]")
+lines = File.read(path).split("\n")
+
+bss_items_header = ['id', 'size', 'path']
+bss_items = []
+
+# parse bss items
+lines.each_with_index do |line, i|
+  match = line.match(/^\s*\.bss\.(\w*)/)
+  next unless match
+  id = match[1].chomp
+  next if id.empty?
+
+  match = lines[i+1].match(/\s*0x(\w*)\s*0x(\w*)\s*(.*)/)
+  next unless match
+  size = match[2]
+  path = match[3]
+  next if size.empty?
+  # hex string to int
+  size = size.to_i(16)
+
+  bss_items << [id, size, path]
+end
+
+# sort by size
+sorted = bss_items.sort do |item1, item2|
+  item2[1] <=> item1[1]
+end
+
+puts Terminal::Table.new(rows: [bss_items_header].concat(sorted))
