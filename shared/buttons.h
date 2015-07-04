6ab63f71211aad3d3349d5968f4f3692f045f20d
ximus <maximeliron@gmail.com>
2015-07-03 18:56:03 -0700

    Fix buttons.h signatures

--- shared/buttons.h
+++ shared/buttons.h
@@ -3,17 +3,17 @@
 #define BUTTON_FLAGS (GPIOINT_DEBOUNCE | GPIOINT_FALLING_EDGE)
 
 
-static inline on_button1(fp_irqcb callback)
+static inline void on_button1(fp_irqcb callback)
 {
     gpioint_set(1, BIT6, BUTTON_FLAGS, callback);
 }
 
-static inline on_button2(fp_irqcb callback)
+static inline void on_button2(fp_irqcb callback)
 {
     gpioint_set(1, BIT5, BUTTON_FLAGS, callback);
 }
 
-static inline on_button3(fp_irqcb callback)
+static inline void on_button3(fp_irqcb callback)
 {
     gpioint_set(1, BIT4, BUTTON_FLAGS, callback);
 }
